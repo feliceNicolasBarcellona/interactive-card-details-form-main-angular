40d13a59d74e66c462e63734ab89aeb62405ac16
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2023-10-23 14:18:48 +0200

    add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interactive-card-details-form-main-angular';
+  submitted: boolean = false;
+
+  creditCard!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.creditCard = this.formBuilder.group({
+      cardholderName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]*$/)]],
+      cardNumber: ['', [Validators.required, Validators.pattern(/\b(?:\d{4}[-\s]?){4}\b/)]],
+      month: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])$/)]],
+      year: ['', [Validators.required, Validators.pattern(/^[0-9]{2}$/)]],
+      cvc: ['', [Validators.required, Validators.pattern(/^[0-9]{3}$/)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.creditCard.valid) {
+      this.submitted =  true
+    } else {
+      this.creditCard.markAllAsTouched()
+    }
+  }
+
+  onContinue(){
+    this.submitted = false;
+    this.creditCard.reset()
+  }
 }
