8df05a9b39b96121119d796b59fe2f3c78ac089d
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2023-10-25 14:30:03 +0200

    switch to rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,20 @@ import { startWith, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   submitted: boolean = false;
 
   creditCard: FormGroup = this.formBuilder.group({
-    cardholderName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]*$/)]],
-    cardNumber: ['', [Validators.required, Validators.pattern(/^\d{4}\s\d{4}\s\d{4}\s\d{4}$/)]],
+    cardholderName: [
+      '',
+      [Validators.required, Validators.pattern(/^[a-zA-Z ]*$/)],
+    ],
+    cardNumber: [
+      '',
+      [Validators.required, Validators.pattern(/^\d{4}\s\d{4}\s\d{4}\s\d{4}$/)],
+    ],
     month: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])$/)]],
     year: ['', [Validators.required, Validators.pattern(/^[0-9]{2}$/)]],
     cvc: ['', [Validators.required, Validators.pattern(/^[0-9]{3}$/)]],
@@ -21,55 +27,55 @@ export class AppComponent {
   cardholderName$ = this.creditCard.get('cardholderName')?.valueChanges.pipe(
     startWith(''),
     map((cardholderName) => {
-      if(!cardholderName) return 'Jane Appleseed';
-      return cardholderName
+      if (!cardholderName) return 'Jane Appleseed';
+      return cardholderName;
     })
-  )
+  );
 
   cardNumber$ = this.creditCard.get('cardNumber')?.valueChanges.pipe(
     startWith(''),
     map((cardNumber) => {
-      if(!cardNumber) return '0000 0000 0000 0000';
-      return cardNumber
+      if (!cardNumber) return '0000 0000 0000 0000';
+      return cardNumber;
     })
-  )
+  );
 
   month$ = this.creditCard.get('month')?.valueChanges.pipe(
     startWith(''),
     map((month) => {
-      if(!month) return '00';
-      return month
+      if (!month) return '00';
+      return month;
     })
-  )
+  );
 
   year$ = this.creditCard.get('year')?.valueChanges.pipe(
     startWith(''),
     map((year) => {
-      if(!year) return '00';
-      return year
+      if (!year) return '00';
+      return year;
     })
-  )
+  );
 
   cvc$ = this.creditCard.get('cvc')?.valueChanges.pipe(
     startWith(''),
     map((cvc) => {
-      if(!cvc) return '00';
-      return cvc
+      if (!cvc) return '000';
+      return cvc;
     })
-  )
+  );
 
   constructor(private formBuilder: FormBuilder) {}
 
   onSubmit() {
     if (this.creditCard.valid) {
-      this.submitted =  true
+      this.submitted = true;
     } else {
-      this.creditCard.markAllAsTouched()
+      this.creditCard.markAllAsTouched();
     }
   }
 
-  onContinue(){
+  onContinue() {
     this.submitted = false;
-    this.creditCard.reset()
+    this.creditCard.reset();
   }
 }
